48b184916ae578e9a1f745cd8be0a766c6982460
larrytu327 <larrytu327@gmail.com>
2023-08-09 21:57:03 -0700

    updated restaurantsSchema and added seed.js with APIcall to Yelp API

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,9 +1,51 @@
 const mongoose = require("mongoose");
 
 const restaurantsSchema = new mongoose.Schema({
+    id: String,
+    alias: String,
     name: String,
-    type: String,
-    city: String,
+    image_url: String,
+    is_closed: Boolean,
+    url: String,
+    review_count: Number,
+    categories: [
+      {
+        alias: String,
+        title: String
+      }
+    ],
+    rating: Number,
+    coordinates: {
+      latitude: Number,
+      longitude: Number
+    },
+    transactions: [
+      String
+    ],
+    price: String,
+    location: {
+      address1: String,
+      address2: {
+          type: String,
+          default: null
+      },
+      address3: {
+          type: String,
+          default: null
+      },
+      city: String,
+      zip_code: String,
+      country: String,
+      state: String,
+      display_address: [
+          String,
+          String,
+          String
+      ]
+    },
+    phone: String,
+    display_phone: String,
+    distance: Number          
 });
 
 const Restaurants = mongoose.model("restaurant", restaurantsSchema);
---
+++ seed.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+// const { MONGODB_URI, APIKey } = process.env;
+const connectionString = process.env.MONGODB_URI;
+const APIKey = process.env.APIKey;
+
+const axios = require('axios');
+
+const mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+mongoose.connection
+    .on("open", () => console.log("This is my awesome amazing connection"))
+    .on("close", () => console.log("You are disconnected from mongoose :'("))
+    .on("error", (error) => console.log(error));
+
+const { Restaurants } = require('./models');
+
+const seedingData = async () => {
+    try {
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: APIKey
+            }
+          };
+        //this fetch is for San Francisco with limit of 50 businesses  
+        const myRestaurants = await fetch('https://api.yelp.com/v3/businesses/search?location=San%2520Francisco&sort_by=best_match&limit=1000', options)
+            .then(response => response.json())
+            // .then(response => console.log(response))
+            .catch(err => console.error(err));
+        // console.log(myBusinesses.businesses[0]);
+        // const allBusinesses = await myBusinesses.json();
+        // const deletedRestaurants = await Restaurants.deleteMany({});
+        const addedRestaurants = await Restaurants.insertMany(myRestaurants.businesses);
+        // console.log(deletedRestaurants);
+        console.log(addedRestaurants);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+seedingData();
