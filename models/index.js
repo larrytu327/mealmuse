1993501932306293c89275ebeb07d5d9a4568bd2
larrytu327 <larrytu327@gmail.com>
2023-08-15 22:24:13 -0700

    changes to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 require('../config/connection');
+const path = require("path");
 
 module.exports = {
-    Restaurants: require('./Restaurants'),
-    User: require('./User'),
-    MyRestaurants: require('./MyRestaurants')
-}
+    Restaurants: require(path.join(__dirname, "Restaurants")),
+    User: require(path.join(__dirname, "User")),
+    MyRestaurants: require(path.join(__dirname, "MyRestaurants"))
+};
