c54e424664daa05f4f6da752f5b24000cb98cbcd
larrytu327 <larrytu327@gmail.com>
2023-08-15 22:32:50 -0700

    changed order for module.exports

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ require('../config/connection');
 const path = require("path");
 
 module.exports = {
+    MyRestaurants: require(path.join(__dirname, "MyRestaurants")),
     Restaurants: require(path.join(__dirname, "Restaurants")),
-    User: require(path.join(__dirname, "User")),
-    MyRestaurants: require(path.join(__dirname, "MyRestaurants"))
+    User: require(path.join(__dirname, "User"))
 };
