0215e66ab55954a9351a884fef4c048cc7d52c8b
larrytu327 <larrytu327@gmail.com>
2023-08-18 22:42:38 -0700

    updated cors to allow access by frontend

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const morgan = require("morgan");
 ///////////////////////////////
 // MiddleWare
 ////////////////////////////////
-app.use(cors()); // to prevent cors errors, open access to all origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+})); // to prevent cors errors, open access to all origins
 app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
 
